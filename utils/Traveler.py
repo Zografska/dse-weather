c4b14f1c39f4f5da4a63162670ce11b80b04cc00
Aleksandra Zografska <aleksndra2@gmail.com>
2024-11-16 17:16:20 +0100

    fix: searching for los angeles located in Chile by mistake + clean

--- utils/Traveler.py
+++ utils/Traveler.py
@@ -12,15 +12,7 @@ temperature_by_city_data = dh.DataHandler("./data/GlobalLandTemperaturesByCity.c
 temperature_by_city_data.clean(feature="Latitude")
 temperature_by_city_data.clean(feature="Longitude")
 traveler = Traveler("Peking")
-# temperature_by_city_data.clean(feature="AverageTemperature")
 temperature_by_city_data.clean(feature="City")
-
-# china_city = temperature_by_city_data.dataframe[
-#     temperature_by_city_data.dataframe["City"] == "Shunyi"
-# ].sort_values("dt")
-# print(china_city.tail(10))
-
-# print(temperature_by_city_data.dataframe["City"].unique().__len__())
 
 # transform the data
 temperature_by_city_data.dataframe["Latitude"] = temperature_by_city_data.dataframe[
@@ -32,7 +24,8 @@ temperature_by_city_data.dataframe["Longitude"] = temperature_by_city_data.dataf
 
 goal = (
     temperature_by_city_data.dataframe.where(
-        temperature_by_city_data.dataframe["City"] == "Los Angeles"
+        (temperature_by_city_data.dataframe["City"] == "Los Angeles")
+        & (temperature_by_city_data.dataframe["Country"] == "United States")
     )
     .dropna()
     .reset_index()
@@ -61,12 +54,9 @@ starting_point = (
 
 starting_location = starting_point.iloc[0]
 
-# print(starting_location)
 grouped = temperature_by_city_data.dataframe.groupby(
     temperature_by_city_data.dataframe["dt"]
 )["City"]
-
-# print(grouped.nunique())
 
 
 # counting the unique cities
@@ -80,25 +70,10 @@ travel_dataset = temperature_by_city_data.dataframe
 
 mask = travel_dataset["dt"] == "2013-08-01"
 travel_dataset = travel_dataset[mask]
-# print(travel_dataset)
-# starting_location = [float(x[:-1]) for x in starting_location_raw]
-
-# ne raboti
-# three_closest = travel_dataset.sort_values(
-#     by=["Latitude", "Longitude"],
-#     key=lambda x: (
-#         calculateDistance(
-#             x.Latitude,
-#             x.Longitude,
-#             1,
-#             2,
-#         )
-#     ),
-# )
 
 
 # get first tree closest
-def get_neighbours(dataset, location):
+def get_neighbours(dataset, location, goal):
     dataset = dataset.copy()
     dataset["distance"] = dataset.apply(
         lambda row: calculateDistance(
@@ -120,38 +95,25 @@ def get_neighbours(dataset, location):
         axis=1,
     )
 
-    # destination = dataset[
-    #     (dataset["City"] == "Los Angeles") & (dataset["distance"] == 0)
-    # ]
-    # if destination.__len__():
-    #     return [destination, "arrived"]
-
-    if location["Country"] == "United States":
-        destination = dataset[(dataset["Country"] == "United States")]
-        if destination.__len__():
-            return [destination, "arrived"]
+    has_arrived = dataset[
+        (dataset["City"] == "Los Angeles") & (dataset["distance"] == 0)
+    ]
+    if has_arrived.__len__():
+        return [has_arrived, "arrived"]
 
     dataset = dataset[
         (dataset["City"] != location["City"]) & (dataset["distance"] != 0)
     ]
-    # force to go east
-    # dataset["TransformedLongitude"] = np.where(
-    #     dataset["Longitude"] > 0, dataset["Longitude"], -dataset["Longitude"]
-    # )
-    # dataset = dataset[dataset["Longitude"] > 0]
+
     dataset = dataset[dataset["Latitude"] > 33.5]
     dataset = dataset[dataset["distance_to_goal"] < location["distance_to_goal"]]
 
     dataset = dataset.sort_values(
-        by=["distance", "distance_to_goal", "AverageTemperature"],
-        ascending=[True, True, False],
+        by=["distance", "AverageTemperature"],
+        ascending=[True, False],
     ).reset_index()
 
-    return [dataset.head(5), "travelling"]
-
-
-# travel_dataset["Visited"] = "No"
-[neighbours, status] = get_neighbours(travel_dataset, starting_location)
+    return [dataset.head(3), "travelling"]
 
 
 ## find path to destination bfs
@@ -165,7 +127,13 @@ def bfs(travel_dataset, node):
 
     while queue:
         [current, path, visited] = queue.pop(0)
-        [neighbours, status] = get_neighbours(travel_dataset, current)
+        [neighbours, status] = get_neighbours(travel_dataset, current, goal)
+
+        if current["City"] == "Los Angeles":
+            print(neighbours)
+            print(path)
+            return path
+
         if current["distance_to_goal"] < closest:
             closest = current["distance_to_goal"]
             queue = [node for node in queue if node[0]["distance_to_goal"] <= closest]
@@ -176,16 +144,11 @@ def bfs(travel_dataset, node):
         if current["Country"] == "United States":
             print(f"I stepped in the US in city {current['City']}")
             print(neighbours)
-            queue.clear()
+            queue = [node for node in queue if node[0]["Country"] == "United States"]
 
         print(
-            f"i am travelling from {current['City']}, already passed {','.join(visited)} cities, distance to goal is {current['distance_to_goal']}"
+            f"i am travelling from {current['City']}, already passed {','.join(path)} cities, distance to goal is {current['distance_to_goal']}"
         )
-        if current["City"] == "Los Angeles":
-            print(neighbours)
-            print(path)
-            print(visited)
-            return path
 
         for _, neighbour in neighbours.iterrows():
             if neighbour["City"] not in path:
