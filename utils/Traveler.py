4debf9b33927569002268706dc2d54a10b1cd42d
Aleksandra Zografska <aleksndra2@gmail.com>
2024-11-16 17:32:39 +0100

    more cleanup, remove unused 'visited' variable + add some comments

--- utils/Traveler.py
+++ utils/Traveler.py
@@ -22,6 +22,14 @@ temperature_by_city_data.dataframe["Longitude"] = temperature_by_city_data.dataf
     "Longitude"
 ].transform(lambda x: transform_longitude(x))
 
+date_of_travel = "2013-08-01"
+
+travel_dataset = temperature_by_city_data.dataframe.copy()
+# get data for the date of travel
+travel_dataset = travel_dataset[travel_dataset["dt"] == date_of_travel]
+
+
+# find the goal in the dataset
 goal = (
     temperature_by_city_data.dataframe.where(
         (temperature_by_city_data.dataframe["City"] == "Los Angeles")
@@ -31,6 +39,8 @@ goal = (
     .reset_index()
 ).iloc[0]
 
+# create a new column with the distance to the goal
+# this will be used as a heuristic so it could reach the goal faster
 temperature_by_city_data.dataframe["distance_to_goal"] = (
     temperature_by_city_data.dataframe.apply(
         lambda row: calculateDistance(
@@ -49,30 +59,12 @@ starting_point = (
     )
     .dropna()
     .reset_index()
-)
+).iloc[0]
 
 
-starting_location = starting_point.iloc[0]
-
-grouped = temperature_by_city_data.dataframe.groupby(
-    temperature_by_city_data.dataframe["dt"]
-)["City"]
-
-
-# counting the unique cities
-# grouping the data by date and filtering on unique 'city' values
-# we see that on 2013-09-01 we have the same count of records
-# as number of unique cities
-# so we can choose that year as our travel year
-
-
-travel_dataset = temperature_by_city_data.dataframe
-
-mask = travel_dataset["dt"] == "2013-08-01"
-travel_dataset = travel_dataset[mask]
-
-
-# get first tree closest
+# get the three closest neighbours or the goal if it is reached
+# the neighbours are sorted by distance and then by temperature
+# heuristic: neighbours that are further away from the goal are discarded
 def get_neighbours(dataset, location, goal):
     dataset = dataset.copy()
     dataset["distance"] = dataset.apply(
@@ -95,39 +87,39 @@ def get_neighbours(dataset, location, goal):
         axis=1,
     )
 
-    has_arrived = dataset[
+    at_destination = dataset[
         (dataset["City"] == "Los Angeles") & (dataset["distance"] == 0)
     ]
-    if has_arrived.__len__():
-        return [has_arrived, "arrived"]
+    if at_destination.__len__():
+        return at_destination
 
     dataset = dataset[
         (dataset["City"] != location["City"]) & (dataset["distance"] != 0)
     ]
 
+    # don't wander off below Lattitude 33.5 since L.A. is at 34.05
     dataset = dataset[dataset["Latitude"] > 33.5]
     dataset = dataset[dataset["distance_to_goal"] < location["distance_to_goal"]]
 
+    # sort by distance to the current city and then by temperature
     dataset = dataset.sort_values(
         by=["distance", "AverageTemperature"],
         ascending=[True, False],
     ).reset_index()
 
-    return [dataset.head(3), "travelling"]
+    return dataset.head(3)
 
 
 ## find path to destination bfs
 def bfs(travel_dataset, node):
-    visited = []
-    visited.append(node["City"])
     path = []
     queue = []
-    queue.append([node, [node["City"]], []])
+    queue.append([node, [node["City"]]])
     closest = node["distance_to_goal"]
 
     while queue:
-        [current, path, visited] = queue.pop(0)
-        [neighbours, status] = get_neighbours(travel_dataset, current, goal)
+        [current, path] = queue.pop(0)
+        neighbours = get_neighbours(travel_dataset, current, goal)
 
         if current["City"] == "Los Angeles":
             print(neighbours)
@@ -153,8 +145,8 @@ def bfs(travel_dataset, node):
         for _, neighbour in neighbours.iterrows():
             if neighbour["City"] not in path:
                 city_name = current["City"]
-                queue.append([neighbour, path + [city_name], visited + [city_name]])
+                queue.append([neighbour, path + [city_name]])
 
 
-path = bfs(travel_dataset, starting_location)
+path = bfs(travel_dataset, starting_point)
 print(f"Path to Los Angeles is {path}")
